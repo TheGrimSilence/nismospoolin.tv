b143548fd3990f26f0b26195129c1a6d6f3c7774
Grim <silencegrim@gmail.com>
2021-11-05 02:11:19 -0400

    fix: extraneous and broken import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import Card from '../components/Card';
 import Container from '../components/Container';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
